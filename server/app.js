412a4e931488d86376cd22407d56bf404aea423c
Katie Roy <kmr8172@g.rit.edu>
2020-03-30 17:35:37 -0400

    fixed eslint errors

--- server/app.js
+++ server/app.js
@@ -11,20 +11,20 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
 
-//Setup mongoose options to use newer functionality
+// Setup mongoose options to use newer functionality
 const mongooseOptions = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 
 mongoose.connect(dbURL, mongooseOptions, (err) => {
-    if (err) {
-        console.log('Could not connect to database');
-        throw err;
-    }
+  if (err) {
+    console.log('Could not connect to database');
+    throw err;
+  }
 });
 
-//Pull in our routes
+// Pull in our routes
 const router = require('./router.js');
 
 const app = express();
@@ -32,7 +32,7 @@ app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
 app.use(bodyParser.urlencoded({
-    extended: true,
+  extended: true,
 }));
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
@@ -42,8 +42,8 @@ app.use(cookieParser());
 router(app);
 
 app.listen(port, (err) => {
-    if (err) {
-        throw err;
-    }
-    console.log(`Listening on port ${port}`);
+  if (err) {
+    throw err;
+  }
+  console.log(`Listening on port ${port}`);
 });
